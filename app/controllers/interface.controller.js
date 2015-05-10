f0f1cd5dae8dc82e4cf0397f8f710917ac9e0282
Robert Magnusson <robert.magnusson@oddhill.se>
2015-05-10 23:37:58 +0200

    Added additional comments for interfaceCtrl

--- app/controllers/interface.controller.js
+++ app/controllers/interface.controller.js
@@ -4,12 +4,12 @@
 robotApp.controller('interfaceCtrl', function($scope) {
   // Create array of coordinates
   $scope.coords = [
-    {"pos": "x", "val" : 1},
-    {"pos": "y", "val" : 2}
+    {"pos": "x", "val" : 0}, // Coordinates - X
+    {"pos": "y", "val" : 0}  // Coordinates - Y
   ];
 
   // Global variables
-  $scope.orient = 1;    // Initial orientation
+  $scope.orient = 1;   // Initial orientation
   var gridWidth = 5;   // Set width of grid
   var gridHeight = 5;  // Set height of grid
 
@@ -17,53 +17,58 @@ robotApp.controller('interfaceCtrl', function($scope) {
   $scope.submit = function() {
     // Handle the input string from submited form
     if ($scope.movement) {
-      var str = $scope.movement.toLowerCase();
-      var res = str.split("");
-      var resultString = "";
+      var str = $scope.movement.toLowerCase(); // Make the string lowercase
+      var res = str.split("");                 // Explode string on every character
+      var resultString = "";                   // Declare new string for movement
 
+      // Loop through the entire input
       for (var i = 0; i < res.length;  i++) {
+        // Handle turn-left commands
         if(res[i] == 'v' || res[i] == 'l') {
-          $scope.orient--;
-          // Loop the variable string
+          $scope.orient--;  // decrease orientation
+          // If the orientation goes from north to west
           if($scope.orient == 0) {
             $scope.orient = 4;
           }
         }
 
+        // Handle turn-right commands
         if (res[i] == 'h' || res[i] == 'r') {
-          $scope.orient++;
-          // Loop the variable string
+          $scope.orient++; // increase orientation
+          // If the orientation goes from west to north
           if ($scope.orient == 5) {
             $scope.orient = 1;
           };
         };
 
+        // On every move command add the orientation to the movement string
         if (res[i] == 'g' || res[i] == 'f') {
           resultString += $scope.orient;
         };
       }
 
-      // Loop through the movement string
+      // Loop through the entire movement string
       for (var u = 0; u < resultString.length;  u++) {
         // if the robot moves north and is within grid boundries
         if(resultString[u] == '1' && $scope.coords[1]['val'] < gridWidth) {
-          $scope.coords[1]['val']++;
+          $scope.coords[1]['val']++;  // Increase the X Value
         }
         // if the robot moves east and is within grid boundries
         if(resultString[u] == '2' && $scope.coords[0]['val'] < gridHeight) {
-          $scope.coords[0]['val']++;
+          $scope.coords[0]['val']++;  // Increase the Y Value
         }
         // if the robot moves south and is within grid boundries
         if(resultString[u] == '3' && $scope.coords[1]['val'] > 0) {
-          $scope.coords[1]['val']--;
+          $scope.coords[1]['val']--;  // Decrease the Y Value
         }
         // if the robot moves west and is within grid boundries
         if(resultString[u] == '4' && $scope.coords[0]['val'] > 0) {
-          $scope.coords[0]['val']--;
+          $scope.coords[0]['val']--;  // Decrease the X Value
         }
       }
     }
 
+    // Clear the input field after each command submit
     $scope.movement = "";
   }
 });
